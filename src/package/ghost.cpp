50acaccba5414db831735918549f2886825ab43e
Xuwu Ruo <hugo15@sina.com>
2011-11-15 00:15:51 +0800

    change Duojian from frequent to not

--- src/package/ghost.cpp
+++ src/package/ghost.cpp
@@ -44,13 +44,13 @@ class Duojian: public TriggerSkill{
 public:
     Duojian():TriggerSkill("duojian"){
         events   << PhaseChange ;
-        frequency = Frequent;
+
     }
     virtual bool trigger(TriggerEvent , ServerPlayer *player, QVariant &) const{
         Room *room = player->getRoom();
         if(player->getPhase() == Player::Start){
             foreach(ServerPlayer *other, room->getOtherPlayers(player)){
-                if(other->getWeapon() && other->getWeapon()->objectName() == "qinggang_sword"){
+                if(other->getWeapon() && other->getWeapon()->objectName() == "qinggang_sword"&& player->askForSkillInvoke(objectName())){
                     player->obtainCard(other->getWeapon());
                     return false;
                 }
@@ -63,7 +63,7 @@ public:
                       if(judge){
                           int judge_id = judge->getEffectiveId();
                           const Card *card = Sanguosha->getCard(judge_id);
-                          if(card->inherits("QinggangSword")){
+                          if(card->inherits("QinggangSword")&& player->askForSkillInvoke(objectName())){
                               player->obtainCard(card);
                               return false;
                          }
